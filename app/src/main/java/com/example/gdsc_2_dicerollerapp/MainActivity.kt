a18da9e097099d7b4fc7b8895a3c752c56c1dc9e
Abhishek Aggarwal <abhishekaggarwal345@gmail.com>
2022-09-26 23:03:09 +0530

    DiceRoller_Jetpack

---
+++ app/src/main/java/com/example/gdsc_2_dicerollerapp/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.gdsc_2_dicerollerapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.gdsc_2_dicerollerapp.ui.theme.GDSC_2_DiceRollerAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GDSC_2_DiceRollerAppTheme {
+                // A surface container using the 'background' color from the theme
+                ButtonWithTextAndImage()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DiceRollerApp(){
+    ButtonWithTextAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center))
+}
+
+@Composable
+fun ButtonWithTextAndImage(modifier: Modifier = Modifier) {
+    var result by remember {
+        mutableStateOf(1)
+    }
+    val imageResource = when(result){
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    Column(
+        modifier = Modifier,
+        horizontalAlignment = Alignment.CenterHorizontally){
+
+    Image(
+        painter = painterResource(imageResource),
+        contentDescription = result.toString())
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result = (1..6).random() }) {
+         Text(text = "Roll")
+        }
+    }
+}
+
+
